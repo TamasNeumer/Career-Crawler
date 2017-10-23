e833b7a9d3a9755088909713f001810e31c7187f
TamasNeumer <thomasneumer@gmail.com>
2017-10-23 22:52:25 +0200

    Trying to add cli parser

--- src/main/java/JobCrawlerbackend.java
+++ src/main/java/JobCrawlerbackend.java
@@ -1,17 +1,52 @@
-import Scraper.KarriereAtScraper;
+//package com.github.TamasNeumer;
 
+import Scraper.KarriereAtScraper;
+import org.apache.commons.cli.*;
 import java.util.ArrayList;
-import Scraper.JobInfo;
+
 
 public class JobCrawlerbackend {
 
     public static void main(String[] args) {
+        CommandLine cmd = parseArgs(args);
+        if(cmd == null) return;
+
+        String chosenProgrammingLanguage = cmd.getOptionValue("p");
+
         KarriereAtScraper karriereScraper = new KarriereAtScraper();
         ArrayList<String> listOfURLs = karriereScraper.getUrlsOfPagesContainingJobLists();
         for(String pageUrl : listOfURLs)
         {
             System.out.println(karriereScraper.parseJobsOnPage(pageUrl).toString());
-
         }
+    }
+
+    private  static CommandLine parseArgs(String[] args){
+        Options options = new Options();
+
+        Option programmingLanguage = new Option("p", "programmingLanguage",
+                true, "Name of the programming language, that is used to search for jobs.");
+        programmingLanguage.setRequired(true);
+        options.addOption(programmingLanguage);
+
+        Option jsonOutputFile = new Option("j", "jsonOutputFile", true,
+                "Output file containing the full jobs (title + description) in json format.");
+        programmingLanguage.setRequired(false);
+        options.addOption(jsonOutputFile);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try{
+            cmd = parser.parse(options, args);
+            return cmd;
+        } catch (ParseException e){
+            System.out.println(e.getMessage());
+            formatter.printHelp("JobCrawler", options);
+
+            System.exit(1);
+        }
+        return null;
     }
 }
